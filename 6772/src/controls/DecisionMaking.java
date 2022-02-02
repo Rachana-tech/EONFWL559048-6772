cae4efad8b9b02ec78b9e216db0b8a76143c2f47
admin <admin@Eshanvi>
2022-02-02 12:00:12 +0530

    My second commit of controls package

---
+++ 6772/src/controls/DecisionMaking.java
@@ -0,0 +1,12 @@
+package controls;
+
+public class DecisionMaking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Control Statement");
+
+	}
+
+}
